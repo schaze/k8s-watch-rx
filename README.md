fa28cd1317ad6b89e06053477f110079e96197db
schaze <thomas_schaz@gmx.de>
2021-12-19 13:55:53 +0100

    initial commi

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # k8s-watch-rx
 Simple Observable wrapper for kubernetes-client k8s.watch
+
+
+### *Note:*
+
+*This is currently a very basic library. There is no support for any special kubernetes configuration. All is loaded from the default config of the current runtime environment.*
+
+## Basic usage example:
+```typescript
+
+import { watchKubeResourceRx } from "k8s-watch-rx"; 
+import { V1ConfigMap } from "@kubernetes/client-node";
+
+const namespace='default';
+const configMapName='some-config-map';
+
+// watch a configmap in default namesapce
+const watch = watchKubeResourceRx<V1ConfigMap>(`/api/v1/watch/namespaces/${namespace}/configmaps/${configMapName}`);
+
+watch.subscribe({
+    next: event => {
+        console.log('Received event: ', event.phase);
+        console.log('for configmap: ', JSON.stringify(event.apiObj, undefined, 2));
+    }
+});
+
+
+
+```
---
+++ src/index.ts
@@ -0,0 +1,57 @@
+import * as k8s from '@kubernetes/client-node';
+import { Observable } from "rxjs";
+import * as winston from "winston";
+
+let instanceID = 0;
+
+
+export interface KubeWatchEvent<T> {
+    phase: string;
+    apiObj: T;
+    watchObj?: any
+}
+
+/**
+ * Watches a given kubernetes resource for changes and emits KuberWatchEvents.
+ * 
+ * @param resource Name of the resouce e.g. /api/v1/watch/namespaces/<myNamespace>/configmaps/<myConfigmap>
+ * @param kubeConfig Optional kubeConfig options (not implemented yet)
+ * @returns Observable<KubeWatchEvent> emitting events ob changes of resource
+ */
+export function watchKubeResourceRx<T>(resource: string, kubeConfig: any = {}): Observable<KubeWatchEvent<T>> {
+    return new Observable<KubeWatchEvent<T>>((observer) => {
+        const id = instanceID++;
+        const log = winston.child({
+            name: `wk-${instanceID}`,
+            type: 'watchkubeRx',
+        });
+        const kc = new k8s.KubeConfig();
+        kc.loadFromDefault();
+
+        const watch = new k8s.Watch(kc);
+        let watcher = null;
+        let unsubscribed = false;
+
+        watch.watch(resource,
+            {},
+            (phase: string, apiObj: any, watchObj?: any) => {
+                observer.next({ phase: phase, apiObj: apiObj, watchObj: watchObj } as any);
+            },
+            (err: any) => {
+                if (unsubscribed) {
+                    observer.complete()
+                } else {
+                    log.warn(`Warning k8-watcher has completed! For [${resource}]`, err);
+                    observer.error(err);
+                }
+            }).then((watcherRef) => {
+                log.info(`K8-Watcher connected for [${resource}]`);
+                watcher = watcherRef
+            });
+        return () => {
+            unsubscribed = true;
+            watcher.abort();
+        }
+    });
+
+}
